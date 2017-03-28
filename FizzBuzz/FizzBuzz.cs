30bfcb5ae914676851529e73d42021c334d940a0
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-03-28 10:33:04 +0100

    Signed-off-by: Adrian Eyre <adrian.eyre@hotmail.co.uk>

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work
+{
+    public class Kata
+    {
+        public string FizzBuzz(int num)
+        {
+            string result = "";
+            if (num % 3 == 0) result += "fizz";
+            if (num % 5 == 0) result += "buzz";
+            if (result == "") return num.ToString(); else return result;
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work
+{
+    [TestFixture]
+    class FizzBuzzTest
+    {
+        [TestCase]
+        public void FizzBuzz()
+        {
+            Kata test = new Kata();
+            Assert.AreEqual("fizz", test.FizzBuzz(3));
+            Assert.AreEqual("buzz", test.FizzBuzz(5));
+            Assert.AreEqual("fizzbuzz", test.FizzBuzz(15));
+            Assert.AreEqual("1", test.FizzBuzz(1));
+            Assert.AreEqual("2", test.FizzBuzz(2));
+        }
+    }
+}
